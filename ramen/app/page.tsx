849d75e7deb16d9f35fa05dd92b9927f72eb6c9c
Viktor Ritsvall <vritsvall@MacBook-Pro-som-tillhor-Viktor.local>
2026-04-28 07:51:35 +0200

    lägg till tacksida

--- ramen/app/page.tsx
+++ ramen/app/page.tsx
@@ -333,10 +333,10 @@ export default function Home() {
                     width="600"
                     height="450"
                     frameborder="0"
-                    style="border: 1px solid #bfcbda88; border-radius: 4px;"
+                    style={{ border: "1px solid #bfcbda88", borderRadius: "4px" }}  
                     allow="fullscreen; payment"
                     aria-hidden="false"
-                    tabindex="0"
+                    tabIndex={0}
                   ></iframe>
                   </div>
                 </div>
